085ab707b876d743c7e1359f3d3b5aa55d1a7e41
Giordano <giordano.mendes94@gmail.com>
2022-10-19 18:24:49 -0300

    requirement01

--- src/context/TableProvider.js
+++ src/context/TableProvider.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import TableContext from './TableContext';
 
-export default function FormProvider({ children }) {
-  return <TableContext.Provider>{children}</TableContext.Provider>;
+export default function TableProvider({ children }) {
+  const [dataAPI, setDataAPI] = useState([]);
+
+  const fetchAPI = async () => {
+    try {
+      const url = 'https://swapi.dev/api/planets';
+      const response = await fetch(url);
+      const { results } = await response.json();
+      results.filter((planet) => planet.residents && delete planet.residents);
+      setDataAPI(results);
+      console.log(results);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  const contextValue = useMemo(
+    () => ({
+      dataAPI,
+    }),
+    [dataAPI],
+  );
+
+  return (
+    <TableContext.Provider value={ contextValue }>
+      {children}
+    </TableContext.Provider>
+  );
 }
 
-FormProvider.propTypes = {
+TableProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
