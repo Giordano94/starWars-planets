59c5097ce77a7c0d6a91d5294a38d24ef001e9ba
Giordano <giordano.mendes94@gmail.com>
2022-10-21 16:51:52 -0300

    requirement03

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,10 @@ import Context from '../context/Context';
 
 export default function Form() {
   const { filterName, handleFilterByName } = useContext(Context);
+  const { filterColumn, handleFilterByColumn } = useContext(Context);
+  const { filterValue, handleFilterByValue } = useContext(Context);
+  const { filterComparison, handleFilterByComparison } = useContext(Context);
+  const { handleClickFilter } = useContext(Context);
   return (
     <form action="">
       <input
@@ -11,6 +15,51 @@ export default function Form() {
         value={ filterName }
         onChange={ ({ target }) => handleFilterByName(target.value) }
       />
+
+      <label htmlFor="column-filter">
+        Colum
+        <select
+          data-testid="column-filter"
+          value={ filterColumn }
+          onChange={ ({ target }) => handleFilterByColumn(target.value) }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+      </label>
+
+      <label htmlFor="comparison-filter">
+        Operator
+        <select
+          data-testid="comparison-filter"
+          value={ filterComparison }
+          onChange={ ({ target }) => handleFilterByComparison(target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+      </label>
+
+      <label htmlFor="value-filter">
+        <input
+          type="number"
+          data-testid="value-filter"
+          value={ filterValue }
+          onChange={ ({ target }) => handleFilterByValue(target.value) }
+        />
+      </label>
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleClickFilter }
+      >
+        FILTRAR
+      </button>
     </form>
   );
 }
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,14 +1,57 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
 export default function Provider({ children }) {
   const [dataAPI, setDataAPI] = useState([]);
   const [filterName, setFilterName] = useState('');
+  const [filterColumn, setFilterColumn] = useState('population');
+  const [filterValue, setFilterValue] = useState(0);
+  const [filterComparison, setFilterComparison] = useState('maior que');
 
   const handleFilterByName = (name) => {
     setFilterName(name);
   };
+
+  const handleFilterByColumn = (column) => {
+    setFilterColumn(column);
+  };
+
+  const handleFilterByValue = (value) => {
+    setFilterValue(value);
+  };
+
+  const handleFilterByComparison = (comparison) => {
+    setFilterComparison(comparison);
+  };
+
+  const handleClickFilter = useCallback(() => {
+    switch (filterComparison) {
+    case 'maior que': {
+      return setDataAPI(
+        dataAPI.filter(
+          (el) => Number(el[filterColumn]) > Number(filterValue),
+        ),
+      );
+    }
+    case 'menor que': {
+      return setDataAPI(
+        dataAPI.filter(
+          (el) => Number(el[filterColumn]) < Number(filterValue),
+        ),
+      );
+    }
+    case 'igual a': {
+      return setDataAPI(
+        dataAPI.filter(
+          (el) => Number(el[filterColumn]) === Number(filterValue),
+        ),
+      );
+    }
+    default:
+      return dataAPI;
+    }
+  }, [filterColumn, filterComparison, filterValue, dataAPI]);
 
   const fetchAPI = async () => {
     try {
@@ -31,9 +74,23 @@ export default function Provider({ children }) {
     () => ({
       dataAPI,
       filterName,
+      filterValue,
+      filterColumn,
+      filterComparison,
       handleFilterByName,
+      handleFilterByValue,
+      handleFilterByColumn,
+      handleFilterByComparison,
+      handleClickFilter,
     }),
-    [dataAPI, filterName],
+    [
+      dataAPI,
+      filterName,
+      filterColumn,
+      filterValue,
+      filterComparison,
+      handleClickFilter,
+    ],
   );
 
   // console.log(contextValue);
